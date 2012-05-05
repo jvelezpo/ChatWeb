51d42b7f3c58d4726a52afad7bc376334ccedece
jvelezpo <sebasvelez@gmail.com>
2012-05-05 09:44:16 -0500

    Chat grueso en Dos

---
+++ Dos/ChatGrueso/src/chatgrueso/JDBCConnection.java
@@ -0,0 +1,32 @@
+package chatgrueso;
+
+import java.sql.DriverManager;
+import com.mysql.jdbc.Connection;
+
+public class JDBCConnection {
+
+    private static Connection connection;
+
+    public static Connection getConexion() throws Exception{
+        if (connection == null)
+            return getJDBConnection();
+        else
+            return connection;
+    }
+
+    private static Connection getJDBConnection() throws Exception{
+        Class.forName("com.mysql.jdbc.Driver");
+        String url = "jdbc:mysql://localhost:3306/chat";
+        connection = (Connection) DriverManager.getConnection(url, "root", "");
+        return connection;
+    }
+
+    public static void cerrarConexion(){
+        try{
+            connection.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
